671027419845f6048451fb31b950ac8f0c1ef46c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-29 22:09:22 -0600

    Add working route to get only one todo

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.get('/todos', async (req, res) => {
 
 app.get('/todos/:id', async (req, res) => {
   try {
-
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1 ", [id]);
+    res.json(todo.rows[0]);
   } catch (err) {
     console.error(err.message)
   }
